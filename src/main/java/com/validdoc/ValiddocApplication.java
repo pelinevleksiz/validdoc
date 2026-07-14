f063da2ec63b38a5fdb938d8e01c93f55a6e21a3
pelinevleksiz <fatmapelinevleksizz@gmail.com>
2026-07-14 10:17:26 +0300

    add docker-compose and database configuration

---
+++ src/main/java/com/validdoc/ValiddocApplication.java
@@ -0,0 +1,13 @@
+package com.validdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValiddocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValiddocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/validdoc/ValiddocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValiddocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
